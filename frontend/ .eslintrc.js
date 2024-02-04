215c72f12d171afe207e951973483e0babacd273
kimhamney <kimhamney@gmail.com>
2024-02-04 14:40:43 +0900

    feat: create frontend

---
+++ frontend/ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    parser: "@babel/eslint-parser",
+  },
+  env: {
+    node: true,
+    browser: true,
+  },
+  extends: ["eslint:recommended", "plugin:vue/vue3-recommended"],
+  rules: {
+    "vue/multi-word-component-names": [
+      "error",
+      {
+        ignores: [],
+      },
+    ],
+  },
+};
